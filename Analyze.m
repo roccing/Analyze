a890d4c3340b096b4846c10ee017244fb646e2e4
Matteo Rocco <36136101+roccing@users.noreply.github.com>
2019-03-02 16:28:55 +0100

    Update Analyze.m
    Main file of the code

--- Analyze.m
+++ Analyze.m
@@ -3,26 +3,26 @@ function [attackTime1,attackTime2, xcross1, xcross2, vectBR1, vectBR2, vectIr1, 
 %Xcrossing2, vector of Brightness 1, vector of brightness 2, vector of
 %spectrum irregularity 1, vector of spectrum irregularity 2, vector of odd1
 %, vector of odd2, vector of even1, vector of even2.
-%Le variabili da settare sono: threshold del taglio(0.10), intervallo
-%xcrossing(500), risoluzione spettro(1/4), 
+%The setting parameters from input are: threshold of the cut(Eg 0.10->10%), frequency spectrum resolution(Eg 1/4), 
 [ch1,ch2,fs]=readWave(file);
-sig1=timeSignalCut(ch1, cutPerc);         %taglio il segnale con threshold 10%
+sig1=timeSignalCut(ch1, cutPerc);         % The input value cutPerc is the threshold percentage that sets the start of the signal
 sig2=timeSignalCut(ch2, cutPerc);
-attackTime1=AttackTime(sig1,fs);       %calcolo attackTime
+attackTime1=AttackTime(sig1,fs);       % I calculate attackTime feature of sound
 attackTime2=AttackTime(sig2,fs);
-xcross1=XCross(sig1, 500);                 %calcolo xcrossing, ampiezza intervallo su cui calcolare l'xcrossing dopo il pto di max
-xcross2=XCross(sig2, 500);
-%divido in 8 parti i canali per fare le analisi frequenziali 
+xcross1=XCross(sig1, 500);                 % I calculate xcrossing,
+xcross2=XCross(sig2, 500);                 % The second parameter is the size of the interval in which the xcrossing is computed, after the max of the array
+%I divide in 8 parts each channel to perform the freq analysis  
 cutVect1=cut8(sig1);
 cutVect2=cut8(sig2);
-logSpectMatr1=spec8(sig1,cutVect1,fs*(coeffResol)); %ho la matrice con gli 8 spettri su 8 righe, decido la risoluzione, ora è 1a4
-logSpectMatr2=spec8(sig2,cutVect2,fs*(coeffResol));
-vectBR1=bright8(logSpectMatr1);  %qui calcolo i vettori di valori di brightness partendo dalla matrice
-vectBR2=bright8(logSpectMatr2);
-vectIr1=spIrr8(logSpectMatr1);  %qui calcolo i vettori di valori di sp Irregularity partendo dalla matrice
-vectIr2=spIrr8(logSpectMatr2);
-[oddVector1,evenVector1]=oddEven8(logSpectMatr1); %calcolo gli odd e even, sono 4 arry in tutto
-[oddVector2,evenVector2]=oddEven8(logSpectMatr2);
+logSpectMatr1=spec8(sig1,cutVect1,fs*(coeffResol)); % I have a matrix with 8 freq. spectrums on its 8 rows, I decide the resolution (Eg 1/4)
+logSpectMatr2=spec8(sig2,cutVect2,fs*(coeffResol));  
+vectBR1=bright8(logSpectMatr1);  % here I compute the vectors of brightness values, staring from the spectrum matrix
+vectBR2=bright8(logSpectMatr2); 
+vectIr1=spIrr8(logSpectMatr1);  %here I compute the vectors of spectrum irregularity values, starting from the matrix
+vectIr2=spIrr8(logSpectMatr2); 
+[oddVector1,evenVector1]=oddEven8(logSpectMatr1); % I compute odd and even components of the spectrum, 4 arrays at all
+[oddVector2,evenVector2]=oddEven8(logSpectMatr2); 
+%---GRAPHIC PART--- NO CALCULATION
 % subplot(6,2,1)
 % plot(ch1)
 % title('ch1')
